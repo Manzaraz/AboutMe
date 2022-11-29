56121da48ee0bbac778ace81fec957baddc8b4d1
Christian Manzaraz <magnusmanz@gmail.com>
2022-11-29 18:59:35 -0300

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Christian Manzaraz on 29/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
